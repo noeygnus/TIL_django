2ca32ecf672cd5d277a06b00c6d678faf5737f06
박성연 <tjddsu3171@naver.com>
2026-04-23 11:12:07 +0900

    0423

--- 10_django_Authentication_03/articles/views.py
+++ 10_django_Authentication_03/articles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Article
 from .forms import ArticleForm
-
 
 def index(request):
     articles = Article.objects.all()
@@ -11,11 +11,11 @@ def index(request):
     return render(request, 'articles/index.html', context)
 
 
+@login_required
 def create(request):
     # 사용자가 form을 작성하고 '제출' 버튼을 눌렀을 때 (POST method) - 기존 create 함수
     if request.method == 'POST':
-        print(request.POST, 9999999999999)
-        form = ArticleForm(request.POST)
+        form = ArticleForm(request.POST, request.FILES)
         if form.is_valid():
             article = form.save()
             return redirect('articles:detail', article.pk)
@@ -39,18 +39,20 @@ def detail(request, article_id):
     return render(request, 'articles/detail.html', context)
 
 
+@login_required
 def delete(request, article_id):
     article = Article.objects.get(pk=article_id)
     article.delete()
     return redirect('articles:index')
 
 
+@login_required
 def update(request, article_id):
     # 1. 수정할 게시글을 DB에서 가져오기 (edit, update 함수 공통)
     article = Article.objects.get(pk=article_id)
 
     if request.method == 'POST':
-        form = ArticleForm(request.POST, instance=article)
+        form = ArticleForm(request.POST, request.FILES, instance=article)
         if form.is_valid():
             form.save()
             return redirect('articles:detail', article.pk)
@@ -63,5 +65,3 @@ def update(request, article_id):
         'article': article, # 어떤 글을 수정하는지 게시글 pk를 URL로 전달하기 위해 필요
     }
     return render(request, 'articles/update.html', context)
-
-
